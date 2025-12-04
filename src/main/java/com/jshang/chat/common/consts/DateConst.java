8b71259708a9baa4cbc96d36643113497e799bf0
ACactus <shangjinv6@163.com>
2025-12-04 22:12:23 +0800

    feat: add DateConst and DateTimeTools utility classes
    Introduce new utility classes for date-related constants and operations to centralize date handling logic in the codebase.

---
+++ src/main/java/com/jshang/chat/common/consts/DateConst.java
@@ -0,0 +1,7 @@
+package com.jshang.chat.common.consts;/**
+ * @Description 
+ * @Date 2025/12/4 21:10
+ * @Author Shawn
+ */
+public class DateConst {
+}
---
+++ src/main/java/com/jshang/chat/service/tools/DateTimeTools.java
@@ -0,0 +1,7 @@
+package com.jshang.chat.service.tools;/**
+ * @Description 
+ * @Date 2025/12/4 21:08
+ * @Author Shawn
+ */
+public class DateTimeTools {
+}
